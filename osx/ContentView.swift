3d6dbb507312c8a83c8ba243ccd8646b16fd9e3e
hyebin <gpqlsdbwls1@naver.com>
2023-05-02 16:11:00 +0900

    swiftUI grid view 만들기

--- osx/ContentView.swift
+++ osx/ContentView.swift
@@ -8,14 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isUpdate: Bool = false
+    let columns = [GridItem(.flexible(),spacing: 4), GridItem(.flexible(),spacing: 4), GridItem(.flexible(),spacing: 4), GridItem(.flexible(),spacing: 4), GridItem(.flexible(),spacing: 4), GridItem(.flexible(),spacing: 4), GridItem(.flexible(),spacing: 4)]
+    let colors: [Color] = [.black, .blue, .cyan, .indigo, .mint, .yellow, .orange]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            LazyVGrid(
+                columns: columns,
+                alignment: .center,
+                spacing: 10,
+                content: {
+                    ForEach(0..<49) { index in
+                        Circle()
+                            .fill(Color.blue)
+                            .frame(width: 20, height: 20)
+                    }
+                })
+            Divider()
+            CircleView(move: MovePose())
+            Button("update") {
+                isUpdate.toggle()
+            }
         }
         .padding()
+    }
+}
+
+struct CircleView: View {
+    @StateObject var move: MovePose
+    
+    var body: some View {
+        VStack {
+            Circle()
+                .fill(Color.blue)
+                .frame(width: 10, height: 10)
+                .offset(y: move.moveY)
+            Button("jump") {
+                move.jump()
+            }
+        }
+    }
+}
+
+class MovePose: ObservableObject {
+    @Published private(set) var moveY: CGFloat = 0
+    
+    init() {
+        print(#function)
+    }
+    
+    func jump() {
+        moveY -= 10
     }
 }
 
