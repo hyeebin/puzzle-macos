20e6facebe9c38fd1ae4f0744fb3c4a2a51ab95c
hyebin <gpqlsdbwls1@naver.com>
2023-05-02 13:30:25 +0900

    Initial Commit

---
+++ osx/osxApp.swift
@@ -0,0 +1,17 @@
+//
+//  osxApp.swift
+//  osx
+//
+//  Created by cubicinc on 2023/05/02.
+//
+
+import SwiftUI
+
+@main
+struct osxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
